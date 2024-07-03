162475884056280bb2f68501cdee1c86fa8a4515
samarbadriddin0v <info@sammi.ac>
2024-07-03 20:04:35 +0300

    Commit

--- Random-User-Generator/Services/UserService.cs
+++ Random-User-Generator/Services/UserService.cs
@@ -7,13 +7,11 @@ public class UserService
 {
   public User GenerateRandomUser()
   {
-    // Define the ObjectFiller for the User class
     var filler = new Filler<User>();
     filler.Setup()
       .OnProperty(u => u.Name).Use(new RealNames(NameStyle.FirstNameLastName))
       .OnProperty(u => u.Email).Use(new EmailAddresses());
 
-    // Generate and return a random user
     return filler.Create();
   }
 }
